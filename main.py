5eaa4c500b9cce961c9225284eeee2b4bce08081
Ciward <108670147+Ciward@users.noreply.github.com>
2024-01-31 11:59:24 +0800

    a

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ done = False
 GRID_WIDTH = 30
 GRID_HEIGHT = 30
 
-input_model_path='/kaggle/input/model4800/model4800'
+#input_model_path='/kaggle/input/model4800/model4800'
 #html_path='/kaggle'
 
 directionNum = {
